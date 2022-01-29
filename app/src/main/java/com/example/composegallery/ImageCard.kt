8360c62aa1613ab0d50c43afe40be5fdbf1f129a
Splintix98 <niklasunruh@gmail.com>
2022-01-29 12:13:55 +0100

    Displaying thumbnails in own composable using GlideImage

---
+++ app/src/main/java/com/example/composegallery/ImageCard.kt
@@ -0,0 +1,111 @@
+package com.example.composegallery
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentCompositionLocalContext
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.skydoves.landscapist.glide.GlideImage
+
+@Composable
+fun ImageCard(
+    context: Context,
+    bitmap: Bitmap,
+    title: String,
+    contentDescription: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .padding(16.dp)
+    ) {
+
+        Card(
+            shape = RoundedCornerShape(15.dp),
+            elevation = 5.dp,
+            modifier = modifier
+                .fillMaxWidth()
+                .clickable {
+                    Toast
+                        .makeText(context, title, Toast.LENGTH_SHORT)
+                        .show()
+                }
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(200.dp)
+            ) {
+                /*Image(
+                    bitmap = bitmap.asImageBitmap(),
+                    contentDescription = contentDescription,
+                    contentScale = ContentScale.Crop
+                )*/
+                GlideImage(
+//                                imageModel = it.contentUri,
+                    imageModel = Image(
+                        bitmap = bitmap.asImageBitmap(),
+                        contentDescription = contentDescription
+                    ),
+                    loading = {
+                        ConstraintLayout(
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            val indicator = createRef()
+                            CircularProgressIndicator(
+                                modifier = Modifier.constrainAs(indicator) {
+                                    top.linkTo(parent.top)
+                                    bottom.linkTo(parent.bottom)
+                                    start.linkTo(parent.start)
+                                    end.linkTo(parent.end)
+                                }
+                            )
+                        }
+                    },
+                    contentScale = ContentScale.FillHeight,
+                )
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            Brush.verticalGradient(
+                                colors = listOf(
+                                    Color.Transparent,
+                                    Color.Black
+                                ),
+                                startY = 300f
+                            )
+                        )
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(12.dp),
+                    contentAlignment = Alignment.BottomStart
+                ) {
+                    Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+                }
+            }
+        }
+    }
+}
